179aa484eeb50eea589e227cc28a887d36e7c3c7
Qianmeng Chen <qianmengchen@outlook.com>
2018-11-02 00:53:27 -0700

    change file directory organization

--- app/components/TaskList/Container.js
+++ app/components/TaskList/Container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
-import TaskList from '../components/TaskList'
-import { fetchTasks } from '../actions';
+import TaskList from './TaskList'
+import { fetchTasks } from '../../actions';
 
 const mapStateToProps = state => ({
     tasks: state.tasks

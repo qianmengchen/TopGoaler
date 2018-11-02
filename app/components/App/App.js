f13defad1131b63a4b3dc79d8d99982c8c453264
Qianmeng Chen <qianmengchen@outlook.com>
2018-11-02 12:35:38 -0700

    change file directory organization (#1)

---
+++ app/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TaskList } from '../TaskList/index';
+import { View } from 'react-native';
+
+const App = () => (
+    <View>
+        <TaskList></TaskList>
+    </View>
+);
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import rootReducer from './reducers'
-import App from './components/App'
+import { App } from './components/App/index'
 
 const loggerMiddleware = createLogger()
 const store = createStore(rootReducer,

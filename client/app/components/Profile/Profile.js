552b9c1227ee2ab41c03eaab7b3cd8156327b263
Qianmeng Chen <qianmengchen@outlook.com>
2018-11-08 20:27:31 -0800

    remove unused variable

--- client/app/components/Profile/Profile.js
+++ client/app/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
-import { Card, Button } from 'react-native-elements';
+import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Profile extends Component {
   render() {

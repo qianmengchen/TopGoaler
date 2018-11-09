827b2c5e023d0e8df7fc584dd80bac0a011161f4
Qianmeng Chen <qianmengchen@outlook.com>
2018-11-09 04:37:45 -0800

    add styles and blocks

--- client/app/components/Profile/Profile.js
+++ client/app/components/Profile/Profile.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Avatar, Button } from 'react-native-elements';
+import {
+  board,
+  personalInfo,
+  descriptions,
+  actions,
+  intro,
+  buttons,
+  introText
+} from './styles';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.fakeProps = {
+      name: 'Joseph Bruin',
+      bio: 'A passionate todo artist'
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Card title="MY PROFILE" image={require('../../images/boy.png')}>
-          <Text>
-            {' '}
-            The Task Tab is now on the '{this.props.currentTaskPage}' page{' '}
-          </Text>
-        </Card>
+      <View style={board.container}>
+        <View style={board.personalInfo}>
+          <View style={personalInfo.descriptions}>
+            <View style={descriptions.intro}>
+              <Text style={introText.name}>{this.fakeProps.name}</Text>
+              <Text style={introText.bio}>{this.fakeProps.bio}</Text>
+            </View>
+            <View style={descriptions.avatar}>
+              <Avatar
+                height={128}
+                width={128}
+                source={require('../../images/boy.png')}
+              />
+            </View>
+          </View>
+          <View style={personalInfo.actions}>
+            <View style={actions.summary}>
+              <Button title="My Summary" buttonStyle={buttons.summary} />
+            </View>
+            <View style={actions.buttons}>
+              <Button title="A" buttonStyle={buttons.A} />
+              <Button title="B" buttonStyle={buttons.B} />
+            </View>
+          </View>
+        </View>
+        <View style={board.channels} />
+        <View style={board.tasks} />
       </View>
     );
   }

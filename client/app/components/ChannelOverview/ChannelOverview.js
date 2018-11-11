ed6e92eb4aaf022960ec4a24ca601eb67f5ba805
Steven <stevenyang0620@hotmail.com>
2018-11-10 22:45:37 -0800

    setup for ChannelOverview

---
+++ client/app/components/ChannelOverview/ChannelOverview.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, SearchBar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {} from './styles';
+import { ChannelOverview } from '.';
+
+export default ChannelOverview;

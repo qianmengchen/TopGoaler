fb8139760beda5a96aceb833c8a82587b696e2b0
Brian Be <bebrian458@gmail.com>
2018-11-12 18:21:33 -0800

    linting check

--- client/app/components/ChannelMemberView/styles.js
+++ client/app/components/ChannelMemberView/styles.js
@@ -65,26 +65,4 @@ const cardRight = StyleSheet.create({
   }
 });
 
-const memberList = StyleSheet.create({
-  container: {
-    borderLeftWidth: 2,
-    borderLeftColor: 'grey',
-    marginHorizontal: 15,
-    marginVertical: 20
-  },
-  text: {
-    color: 'grey',
-    fontSize: 22,
-    paddingLeft: 5
-  },
-  iconList: {
-    flexDirection: 'row',
-    marginHorizontal: 5,
-    marginVertical: 10
-  },
-  icon: {
-    marginHorizontal: 5
-  }
-});
-
 export { header, cardLeft, cardRight };

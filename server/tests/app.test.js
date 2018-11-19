bbc0234494ff828a96238271fe112a64b8d051b5
Jeffrey Sun <zhouhengsun@gmail.com>
2018-11-19 15:44:28 -0800

    Backend test first pass

---
+++ server/tests/app.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const app = require('../app')
+const pool = require('../data/config');
+
+afterEach(() => {
+    pool.end()
+});
+test('should return channel info for channel_creator GET', (done) => {
+    request(app)
+        .get('/channel_creator/Leetcode')
+        .expect(200)
+        .end((_, res) => {
+            expect(res.body).toHaveLength(1)
+            expect(res.body[0]).toHaveProperty('channel', "Leetcode")
+            done()
+        });
+  });
+
+
+test('should create new user', (done) => {
+    request(app)
+        .post('/signup')
+        .send({username:'admin111', password:'admin1'})
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(res => {
+            expect(res.body.status).toBe('success')
+            done()
+        })
+    request(app)
+        .post('/login')
+        .send({username:'admin111', password:'admin1'})
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(res => {
+            expect(res.body.status).toBe('success')
+            done()
+    })
+  });

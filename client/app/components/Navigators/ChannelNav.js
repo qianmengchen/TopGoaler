7b13ca6aa8daf1c4991e11cf58d4eca043915167
Steven <stevenyang0620@hotmail.com>
2018-11-29 16:33:48 -0800

    better navigation

--- client/app/components/Navigators/ChannelNav.js
+++ client/app/components/Navigators/ChannelNav.js
@@ -3,7 +3,6 @@ import { ChannelListPage } from '../ChannelListPage/index';
 import { ChannelPublicView } from '../ChannelPublicView/index';
 import { ChannelMemberView } from '../ChannelMemberView/index';
 import { NewChannelPage } from '../NewChannelPage/index';
-import { PerformancePage } from '../PerformancePage/index';
 import { NewTaskPage } from '../NewTaskPage/index';
 import { ProposalsPage } from '../ProposalsPage/index';
 
@@ -13,7 +12,6 @@ const ChannelNav = createStackNavigator(
     ChannelPublicView: { screen: ChannelPublicView },
     ChannelMemberView: { screen: ChannelMemberView },
     NewChannelPage: { screen: NewChannelPage },
-    PerformancePage: { screen: PerformancePage },
     NewTaskPage: { screen: NewTaskPage },
     ProposalsPage: { screen: ProposalsPage }
   },

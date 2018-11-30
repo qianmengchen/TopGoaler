06cfac0f991e451cc2e62bd193bded63d2f515ee
bingxinzhu <bingxinzhu@g.ucla.edu>
2018-11-29 21:33:58 -0800

    add trycatch to handle error

--- server/routes/create.js
+++ server/routes/create.js
@@ -9,10 +9,14 @@ const pool = require('../data/config');
 // note: create_time is auto generated, so only need two arguments: task_id, user_id
 // curl -d "task_id=1&user_id=2" http://localhost:8001/activity_log
 router.post('/activity_log', (request, response) => {
-    pool.query('INSERT INTO activity_log SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`); 
-        response.status(201).send(`activity_log added with activity_log: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO activity_log SET ?', request.body, (error, result) => { 
+            response.status(201).send(`activity_log added with activity_log: ${result.insertId}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`); 
+    }
+    
 });
 
 
@@ -21,10 +25,13 @@ router.post('/activity_log', (request, response) => {
 // primary key is id, title is a must , other arguments are optional
 // curl -d "title=2&creator=1" http://localhost:8001/channel
 router.post('/channel', (request, response) => {
-    pool.query('INSERT INTO channel SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`);  
-        response.status(201).send(`channel added with channel: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO channel SET ?', request.body, (error, result) => {
+            response.status(201).send(`channel added with channel: ${result.insertId}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`);   
+    }
 });
 
 // post for proposal
@@ -32,10 +39,14 @@ router.post('/channel', (request, response) => {
 // primary key is id, must have: title, channel_id, other arguments are optional
 // curl -d "title=2&channel_id=1" http://localhost:8001/proposal
 router.post('/proposal', (request, response) => {
-    pool.query('INSERT INTO proposal SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`);  
-        response.status(201).send(`proposal added with proposal: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO proposal SET ?', request.body, (error, result) => {
+            response.status(201).send(`proposal added with proposal: ${result.insertId}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`);  
+    }
+    
 });
 
 
@@ -44,10 +55,14 @@ router.post('/proposal', (request, response) => {
 // primary key is id, must have: title, channel_id, other arguments are optional
 // curl -d "channel_id=1&title=bxzhu_task" http://localhost:8001/task
 router.post('/task', (request, response) => {
-    pool.query('INSERT INTO task SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`); 
-        response.status(201).send(`task added with task: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO task SET ?', request.body, (error, result) => {
+            response.status(201).send(`task added with task: ${result.insertId}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`); 
+    }
+
 });
 
 // post for user
@@ -55,10 +70,14 @@ router.post('/task', (request, response) => {
 // primary key is id, must have: name, password, other arguments are optional
 // curl -d "name=bzhu&password=bzhu" http://localhost:8001/user
 router.post('/user', (request, response) => {
-    pool.query('INSERT INTO user SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`);  
-        response.status(201).send(`user added with user: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO user SET ?', request.body, (error, result) => {
+            response.status(201).send(`user added with user: ${result.insertId}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`);   
+    }
+    
 });
 
 
@@ -68,10 +87,14 @@ router.post('/user', (request, response) => {
 // must have: user_id, channel_id
 // curl -d "user_id=1&channel_id=1" http://localhost:8001/user_channel
 router.post('/user_channel', (request, response) => {
-    pool.query('INSERT INTO user_channel SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`); 
-        response.status(201).send(`user_channel added with user_channel: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO user_channel SET ?', request.body, (error, result) => {
+            response.status(201).send(`user_channel added with user_channel: ${result}`);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`); 
+    }
+    
 });
  
 // post for user_task
@@ -79,10 +102,13 @@ router.post('/user_channel', (request, response) => {
 // must have: user_id, task_id
 // curl -d "user_id=1&task_id=1" http://localhost:8001/user_task
 router.post('/user_task', (request, response) => {
-    pool.query('INSERT INTO user_task SET ?', request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`);  
-        response.status(201).send(`user_task added with user_task: ${result.insertId}`);
-    });
+    try {
+        pool.query('INSERT INTO user_task SET ?', request.body, (error, result) => {
+            response.status(201).send(`user_task added with user_task: ${result} `);
+        });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`);   
+    }
 });
 
 // post for vote
@@ -91,19 +117,21 @@ router.post('/user_task', (request, response) => {
 // curl -d "user_id=1&proposal_id=1&score=1 http://localhost:8001/vote
 // return 1 means this proposal is able to change to a task, return 0 means not yet
 router.post('/vote', (request, response) => {
-    const proposal_id = request.body.proposal_id;  
-    pool.query('INSERT INTO vote SET ?',  request.body, (error, result) => {
-        if (error) response.status(401).send(`error create: ${error}`); 
-        //response.status(201).send(`vote added with vote: ${result.insertId}`);
-    });
-    pool.query(` SELECT 
-            (SELECT
-            (SELECT COUNT(*) FROM user_channel WHERE channel_id = (SELECT channel_id FROM proposal WHERE id = ? )) /
-            (SELECT COUNT(*) FROM vote WHERE proposal_id = ? ) 
-            ) <= 2`, [proposal_id, proposal_id], (error, result) => { 
-            if (error) response.status(401).send(`error create: ${error}`);  
-            response.json(result[0]);  
-        }); 
+    try {
+        const proposal_id = request.body.proposal_id;  
+        pool.query('INSERT INTO vote SET ?',  request.body, (error, result) => { 
+        });
+        pool.query(` SELECT 
+                (SELECT
+                (SELECT COUNT(*) FROM user_channel WHERE channel_id = (SELECT channel_id FROM proposal WHERE id = ? )) /
+                (SELECT COUNT(*) FROM vote WHERE proposal_id = ? ) 
+                ) <= 2`, [proposal_id, proposal_id], (error, result) => {  
+                response.json(result[0]);  
+            });
+    } catch (error) {
+        response.status(401).send(`error create: ${error}`); 
+    }
+ 
 });
 
 module.exports = router;
--- server/routes/update.js
+++ server/routes/update.js
@@ -10,12 +10,16 @@ const pool = require('../data/config');
 // curl -X PUT -d "task_id=1" -d "user_id=2" http://localhost:8001/activity_log/1\&1
 
 router.put('/activity_log/:orig_task_id&:orig_user_id', (request, response) => { 
-    const orig_task_id = request.params.orig_task_id; 
-    const orig_user_id = request.params.orig_user_id; 
-    pool.query('UPDATE activity_log SET ? WHERE task_id = ? AND user_id = ?', [request.body, orig_task_id, orig_user_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+    try {
+        const orig_task_id = request.params.orig_task_id; 
+        const orig_user_id = request.params.orig_user_id; 
+        pool.query('UPDATE activity_log SET ? WHERE task_id = ? AND user_id = ?', [request.body, orig_task_id, orig_user_id], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`);  
+    }
+
 });
 
 
@@ -24,13 +28,17 @@ router.put('/activity_log/:orig_task_id&:orig_user_id', (request, response) => {
 // primary key is id, title is a must , 
 // curl -X PUT -d "title=bxzhu_channel" -d "creator=2" http://localhost:8001/channel/bxzhu_channel\&1
 router.put('/channel/:orig_title&:orig_creator', (request, response) => {
-    const orig_title = request.params.orig_title; 
-    const orig_creator = request.params.orig_creator; 
- 
-    pool.query('UPDATE channel SET ? WHERE title = ? AND creator = ?', [request.body, orig_title, orig_creator], (error, result) => {
-        if (error)  response.status(401).json({msg: `cannot update: ${err}`})
-        response.send(result);
-    });
+    try {
+        const orig_title = request.params.orig_title; 
+        const orig_creator = request.params.orig_creator; 
+     
+        pool.query('UPDATE channel SET ? WHERE title = ? AND creator = ?', [request.body, orig_title, orig_creator], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).json({msg: `cannot update: ${err}`}) 
+    }
+    
 });
   
 // UPDATE for proposal
@@ -38,14 +46,17 @@ router.put('/channel/:orig_title&:orig_creator', (request, response) => {
 // primary key is id, must have: title, channel_id, 
 // curl -X PUT -d "title=bxzhu_proposal" -d "channel_id=2" http://localhost:8001/proposal/bxzhu_proposal\&1
 router.put('/proposal/:orig_title&:orig_channel_id', (request, response) => {
+    try {
+        const orig_title = request.params.orig_title; 
+        const orig_channel_id = request.params.orig_channel_id;  
+        pool.query('UPDATE proposal SET ? WHERE title = ? AND channel_id = ?', [request.body, orig_title, orig_channel_id], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        esponse.status(401).send(`unable to update: ${err}`); 
+    }
 
-    const orig_title = request.params.orig_title; 
-    const orig_channel_id = request.params.orig_channel_id; 
- 
-    pool.query('UPDATE proposal SET ? WHERE title = ? AND channel_id = ?', [request.body, orig_title, orig_channel_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+
 });
 
 
@@ -54,14 +65,18 @@ router.put('/proposal/:orig_title&:orig_channel_id', (request, response) => {
 // primary key is id, must have: title, channel_id, other arguments are optional 
 // curl -X PUT -d "title=bxzhu_task" -d "channel_id=2" http://localhost:8001/task/bxzhu_task\&1
 router.put('/task/:orig_title&:orig_channel_id', (request, response) => {
+    try {
+        const orig_title = request.params.orig_title; 
+        const orig_channel_id = request.params.orig_channel_id; 
+     
+        pool.query('UPDATE task SET ? WHERE title = ? AND channel_id = ?', [request.body,orig_title, orig_channel_id], (error, result) => {
+            response.send(result);
+        });       
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`);  
+    }
 
-    const orig_title = request.params.orig_title; 
-    const orig_channel_id = request.params.orig_channel_id; 
- 
-    pool.query('UPDATE task SET ? WHERE title = ? AND channel_id = ?', [request.body,orig_title, orig_channel_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+
 });
 
 
@@ -70,12 +85,16 @@ router.put('/task/:orig_title&:orig_channel_id', (request, response) => {
 // primary key is id, must have: name, password, other arguments are optional 
 // curl -X PUT -d "name=bzhu" -d "password=bzhu" http://localhost:8001/user/bxzhu\&bxzhu_password
 router.put('/user/:orig_name:orig_password', (request, response) => {
-    const orig_name = request.params.orig_name; 
-    const orig_password = request.params.orig_password; 
-    pool.query('UPDATE user SET ? WHERE name = ? AND password = ?', [request.body, orig_name, orig_password], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+    try {
+        const orig_name = request.params.orig_name; 
+        const orig_password = request.params.orig_password; 
+        pool.query('UPDATE user SET ? WHERE name = ? AND password = ?', [request.body, orig_name, orig_password], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`); 
+    }
+
 });
 
 
@@ -84,12 +103,16 @@ router.put('/user/:orig_name:orig_password', (request, response) => {
 // must have: user_id, channel_id
 // curl -X PUT -d "user_id=1" -d "channel_id=1" http://localhost:8001/user_channel/2\&1
 router.put('/user_channel/:orig_user_id:orig_channel_id', (request, response) => {
-    const orig_user_id = request.params.orig_user_id; 
-    const orig_channel_id = request.params.orig_channel_id; 
-    pool.query('UPDATE user_channel SET ? WHERE user_id = ? AND channel_id = ?', [request.body, orig_user_id, orig_channel_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+    try {
+        const orig_user_id = request.params.orig_user_id; 
+        const orig_channel_id = request.params.orig_channel_id; 
+        pool.query('UPDATE user_channel SET ? WHERE user_id = ? AND channel_id = ?', [request.body, orig_user_id, orig_channel_id], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`); 
+        
+    }
 });
 
 // UPDATE for user_task
@@ -97,12 +120,15 @@ router.put('/user_channel/:orig_user_id:orig_channel_id', (request, response) =>
 // must have: user_id, task_id 
 // curl -X PUT -d "user_id=1" -d "task_id=1" http://localhost:8001/user_task/2\&1
 router.put('/user_task/:orig_user_id:orig_task_id', (request, response) => {
-    const orig_user_id = request.params.orig_user_id; 
-    const orig_task_id = request.params.orig_task_id; 
-    pool.query('UPDATE user_task SET ? WHERE user_id = ? AND task_id = ?', [request.body, orig_user_id, orig_task_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`); 
-        response.send(result);
-    });
+    try {
+        const orig_user_id = request.params.orig_user_id; 
+        const orig_task_id = request.params.orig_task_id; 
+        pool.query('UPDATE user_task SET ? WHERE user_id = ? AND task_id = ?', [request.body, orig_user_id, orig_task_id], (error, result) => {
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`);  
+    }
 });
 
 // UPDATE for vote
@@ -110,12 +136,15 @@ router.put('/user_task/:orig_user_id:orig_task_id', (request, response) => {
 // must have: user_id, proposal_id 
 // curl -X PUT -d "user_id=1" -d "proposal_id=1" http://localhost:8001/vote/2\&1
 router.put('/vote/:orig_user_id:orig_proposal_id', (request, response) => {
-    const orig_user_id = request.params.orig_user_id; 
-    const orig_proposal_id = request.params.proposal_id; 
-    pool.query('UPDATE vote SET ?  WHERE user_id = ? AND proposal_id =  ?', [request.body, orig_user_id, orig_proposal_id], (error, result) => {
-        if (error) response.status(401).send(`unable to update: ${err}`);  
-        response.send(result);
-    });
+    try {
+        const orig_user_id = request.params.orig_user_id; 
+        const orig_proposal_id = request.params.proposal_id; 
+        pool.query('UPDATE vote SET ?  WHERE user_id = ? AND proposal_id =  ?', [request.body, orig_user_id, orig_proposal_id], (error, result) => { 
+            response.send(result);
+        });
+    } catch (error) {
+        response.status(401).send(`unable to update: ${err}`);  
+    }
 });
 
 

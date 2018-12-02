80ed9fcd24e9602e581ffc5ab163f55f0e33585b
Brian Be <bebrian458@gmail.com>
2018-12-01 23:01:05 -0800

    completed app test; need container test

---
+++ client/app/tests/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+require('isomorphic-fetch');
+
+import App from '../components/App/App';
+
+const middlewares = [thunkMiddleware]; // you can mock any middlewares here if necessary
+const mockStore = configureStore(middlewares);
+
+const initialTaskState = {
+  tasks: ['do frontend', 'do backend', 'do MVP ftw'],
+  isFetching: false,
+  currentTaskPage: 'ChannelListPage'
+};
+
+describe('Testing App component', () => {
+  const wrapper = shallow(<App loadData={jest.fn()} />, {
+    context: { store: mockStore(initialTaskState) }
+  });
+  const render = wrapper.dive();
+
+  it('renders as expected', () => {
+    expect(render).toMatchSnapshot();
+  });
+});

b6989b49b107d61b9c5d185c3be3c47caca3180c
Brian Be <bebrian458@gmail.com>
2018-12-01 17:33:13 -0800

    WIP memberlist; todo: test nameToInitialMap

--- client/app/tests/MemberList.test.js
+++ client/app/tests/MemberList.test.js
@@ -5,6 +5,7 @@ import thunkMiddleware from 'redux-thunk';
 require('isomorphic-fetch');
 
 import MemberList from '../components/MemberList/MemberList';
+import { Avatar } from 'react-native-elements';
 
 const middlewares = [thunkMiddleware]; // you can mock any middlewares here if necessary
 const mockStore = configureStore(middlewares);
@@ -16,13 +17,35 @@ const initialTaskState = {
 };
 const mockUserIds = [1];
 
+const mockUsers = [
+  {
+    id: 1,
+    name: 'admin',
+    password: 'admin',
+    email: 'this is a email@email.com',
+    avatar_url: 'http://shortlink.in/themes/v3/styles/img/url-link.png'
+  }
+];
+
 describe('Testing Member List Component', () => {
-  const wrapper = shallow(<MemberList user_ids={mockUserIds} users={{}} />, {
-    context: { store: mockStore(initialTaskState) }
-  });
+  const wrapper = shallow(
+    <MemberList user_ids={mockUserIds} users={mockUsers} />,
+    {
+      context: { store: mockStore(initialTaskState) }
+    }
+  );
   const render = wrapper.dive();
 
   it('renders as expected', () => {
     expect(render).toMatchSnapshot();
+  });
+
+  it('generates correct random colors', () => {
+    MemberList.prototype._getRandomColor();
+  });
+
+  it('contains avatar', () => {
+    console.log(wrapper.find(Avatar).debug());
+    expect(wrapper.find(Avatar).exists()).toBe(true);
   });
 });
